148df86722a289a3a39861a6e4b772b55d5905de
Mikhail Kapustin <kapustinomm@gmail.com>
2020-08-12 14:11:54 +0300

    Setup cemetery staff

--- database/seeds/CemeterySeeder.php
+++ database/seeds/CemeterySeeder.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class CemeterySeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-         $this->call(CemeterySeeder::class);
+        factory(App\Models\Cemetery::class, 1)->create();
     }
 }
