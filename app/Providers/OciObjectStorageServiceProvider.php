d6ffce4877f418fec423a8c7873e3b733abb0b49
Mikhail Kapustin <kapustinomm@gmail.com>
2020-08-20 12:22:49 +0300

    Setup boot method for oci object storage service provider

--- app/Providers/OciObjectStorageServiceProvider.php
+++ app/Providers/OciObjectStorageServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use Aws\S3\S3Client;
 use Illuminate\Support\ServiceProvider;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\Filesystem;
+use Illuminate\Support\Facades\Storage;
 
 class OciObjectStorageServiceProvider extends ServiceProvider
 {
@@ -23,6 +27,23 @@ class OciObjectStorageServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (config('filesystems.default') != 'oci') {
+            return;
+        }
+
+        Storage::extend('s3', function($app, $config) {
+            $client = new S3Client([
+                'credentials' => [
+                    'key'    => $config['key'],
+                    'secret' => $config['secret'],
+                ],
+                'region' => $config['region'],
+                'version' => '2006-03-01',
+                'bucket_endpoint' => true,
+                'endpoint' => $config['url']
+            ]);
+
+            return new Filesystem(new AwsS3Adapter($client, $config['bucket'], $config['bucket']));
+        });
     }
 }
