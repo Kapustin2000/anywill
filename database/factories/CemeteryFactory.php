07f36f278508e2732d6426b33c8b7e5c9d5bada0
Mikhail Kapustin <kapustinomm@gmail.com>
2020-09-07 17:05:32 +0300

    Fix cemetery factory

--- database/factories/CemeteryFactory.php
+++ database/factories/CemeteryFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Cemetery::class, function (Faker $faker) {
     return [
-        'name' => $faker->text,
+        'name' => $faker->company,
         'user_id' => function() {
             return factory(App\Models\User::class)->create()->id;
         },
