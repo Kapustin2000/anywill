3f8a4f5379e84cfa7d625d731d8834f55978fb75
Mikhail Kapustin <kapustinomm@gmail.com>
2020-09-22 16:20:08 +0300

    todo organization

--- app/Services/Dto/OrganizationDto.php
+++ app/Services/Dto/OrganizationDto.php
@@ -6,7 +6,7 @@ class OrganizationDto extends AbstractDto implements DtoInterface
 {
 
     /* @var string */
-    public  $classifications, $coordinates, $options, $media;
+    public  $cemeteries, $laboratories, $funeral_homes, $cremations, $address, $media;
 
     /* @return array */
     protected function configureValidatorRules(): array
@@ -23,7 +23,18 @@ class OrganizationDto extends AbstractDto implements DtoInterface
     {
          $this->data = [
             'name' => $data['name'],
+            'owner_type' => User::class,
+            'owner_id' => $data['user_id'] ?? auth()->user()->id,
+            'description' => $data['description'] ?? null
         ];
+
+
+        $this->cemeteries = $data['cemeteries'];
+        $this->laboratories = $data['laboratories'];
+        $this->funeral_homes = $data['funeral_homes'];
+        $this->cremations = $data['cremations'];
+        $this->address = $data['address'];
+        $this->media = $data['media'];
 
         return true;
     }
--- app/Services/OrganizationService.php
+++ app/Services/OrganizationService.php
@@ -6,20 +6,37 @@ use App\Models\Organization;
 use App\Services\Dto\OrganizationDto;
 use App\Services\Interfaces\CemeteryServiceInterface;
 
-Class OrganizationService extends TransactionAbstractService
+Class OrganizationService extends AbstractService
 {
 
     protected $organization;
 
     public function save(OrganizationDto $dto) : Organization
     {
-        return $this->organization = $organization = Organization::create($dto->data);
+         $this->organization = Organization::create($dto->data);
+
+        return $this->persistOrganization($dto);
     }
 
     public function update(OrganizationDto $dto, Organization $organization) : Organization
     {
-        return $this->organization = $organization->update($dto->data);
+        $this->organization = $organization->update($dto->data);
 
+        return $this->persistOrganization($dto);
+    }
+
+    protected function persistOrganization(OrganizationDto $dto)
+    {
+        $this->organization->cemeteries()->sync($dto->cemeteries);
+        $this->organization->laboratories()->sync($dto->laboratories);
+        $this->organization->funeral_homes()->sync($dto->funeral_homes);
+        $this->organization->cremations()->sync($dto->cremations);
+        $this->organization->address()->updateOrCreate($dto->address);
+        $this->persistRelation($this->organization->managers(), $dto->managers);
+        $this->persistRelation($this->organization->media(), $dto->media);
+
+
+        return $this->organization;
     }
 
 } 
