91bfee3404ee4b6c7e007d3d132e66c84076ded1
Mikhail Kapustin <kapustinomm@gmail.com>
2020-09-23 12:23:59 +0300

    Finish options seeding

--- app/Services/AbstractService.php
+++ app/Services/AbstractService.php
@@ -21,7 +21,7 @@ abstract class AbstractService extends TransactionAbstractService
     protected function persistOptions($entity, $options)
     {
         foreach($options as $option) {
-             OptionAble::updateOrCreate(
+            $optionAble = OptionAble::updateOrCreate(
                                [
                                  'service_options_id' => $option['option_id'],
                                  'optionable_type' => get_class($entity),
@@ -33,11 +33,8 @@ abstract class AbstractService extends TransactionAbstractService
                                    'optionable_type' => get_class($entity),
                                    'optionable_id' => $entity->id
                                ]
-                         );
-            $optionAble = OptionAble::where('service_options_id', $option['option_id'])
-                ->where( 'optionable_type' , get_class($entity))
-                ->where('optionable_id', $entity->id)->first();
-
+                         )->first();
+            
             if(isset($option['media'])) $optionAble->media()->sync($option['media'] ?? null);
         }
 
