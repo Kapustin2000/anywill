b8ec386f1c0094ac7f570f985f90d0f4ebe74504
Mikhail Kapustin <kapustinomm@gmail.com>
2020-09-29 12:20:10 +0300

    TODO

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -7,6 +7,11 @@ use Faker\Generator as Faker;
 
 $factory->define(Admin::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'username' => $faker->userName,
+        'email' => $faker->unique()->safeEmail,
+        'email_verified_at' => now(),
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'remember_token' => \Illuminate\Support\Str::random(10),
     ];
 });
