2ac0e6e24f34eaa72761d6abd247c244669c8d07
micle <Kapustin2000>
2020-10-08 14:27:18 +0300

    Todo transactions-types

--- config/transactions.php
+++ config/transactions.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    ['id' => 1, 'name' => 'transfer'],
-    ['id' => 2, 'name' => 'withdrawal'],
-    ['id' => 3, 'name' => 'deposit']
+    'transfer',
+    'withdrawal',
+    'deposit'
 ];
